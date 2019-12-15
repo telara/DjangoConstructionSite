9a1cf542e60fca09b55295d626b810c415a31ac0
taraere <taraelsen@gmail.com>
2019-12-15 18:36:34 +0100

    renaming and max line length to 79 characters

--- building_product/models.py
+++ building_product/models.py
@@ -16,6 +16,7 @@ class BuildingProduct(models.Model):
         ('HBO', 'HBO'),
         ('Opleidingsbedrijf', 'Opleidingsbedrijf'),
     ]
+
     CATEGORY_CHOICES = [
         ('MATERIALEN', 'Materialen'),
         ('CURSUS', 'Cursus'),
@@ -23,30 +24,39 @@ class BuildingProduct(models.Model):
 
     # DATABASE FIELDS
     name = models.CharField(max_length=200)
+
     description = models.TextField()
 
-    photo_thumbnail = ProcessedImageField(upload_to="product_photos",
-                                          processors=[ResizeToFill(500, 500)],
-                                          format='JPEG',
-                                          options={'quality': 200},
-                                          default="product_photos/default_photo.jpg")
-    company_logo_thumbnail = ProcessedImageField(upload_to='company_logos',
-                                                 processors=[
-                                                     ResizeToFill(100, 100)],
-                                                 format='JPEG',
-                                                 options={'quality': 60},
-                                                 default="company_logos/default.jpg")
+    photo = ProcessedImageField(upload_to="product_photos",
+                                processors=[ResizeToFill(500, 500)],
+                                format='JPEG',
+                                options={'quality': 200},
+                                default="product_photos/default_photo.jpg")
+
+    company_logo_thumbnail = ProcessedImageField(
+        upload_to='company_logos',
+        processors=[ResizeToFill(100, 100)],
+        format='JPEG',
+        options={'quality': 60},
+        default="company_logos/default.jpg"
+    )
+
     company_name = models.CharField(max_length=200)
-    categories = MultiSelectField(choices=CATEGORY_CHOICES, default='CURSUS')
-    school_type = models.CharField(
-        max_length=20,
-        choices=SCHOOL_CHOICES,
-        default='MBO'
-    )
-    price = models.DecimalField(
-        max_digits=10, decimal_places=2, default=Decimal('0.00')
-    )
+
+    categories = MultiSelectField(choices=CATEGORY_CHOICES,
+                                  default='CURSUS',
+                                  null=False)
+
+    school_type = models.CharField(max_length=20,
+                                   choices=SCHOOL_CHOICES,
+                                   default='MBO')
+
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                default=Decimal('0.00'))
+
     expiration_date = models.DateTimeField(null=True)
+
     published_date = models.DateTimeField(default=timezone.now)
 
     # SAVE METHOD
